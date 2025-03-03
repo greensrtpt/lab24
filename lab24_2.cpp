8eb8a3be1ccdabc12806414bc894728c96221f20
sireethorn <sireethorn2548.h20p@gmail.com>
2025-03-03 15:13:03 +0700

    lab24_2.cpp

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -23,8 +23,41 @@ int main(){
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
 	
-	//Write your code here
+	// Luffy, Sanji, Nami มาต่อแถวด้านหลัง
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
 	
+	// 2 คนแรก (Alice, Oscar) เข้าไปในสัมมนา (ออกจากแถว)
+	line_up.pop_front();
+	line_up.pop_front();
+	
+	// Naruto มาแทรกแถวหลัง Luffy
+	loc = find(line_up.begin(), line_up.end(), "Luffy");
+	if (loc != line_up.end()) {
+	    ++loc;
+	    line_up.insert(loc, "Narutu");
+	}
+	
+	// Prayath ปวดท้องมาก เลยมาแทรกด้านหน้าแถวสุด
+	line_up.push_front("Prayath");
+	
+	// Tony ยอมไม่ต่อแถว มาด้านหลัง Prayath
+	loc = find(line_up.begin(), line_up.end(), "Prayath");
+	if (loc != line_up.end()) {
+	    ++loc;
+	    line_up.insert(loc, "Tony");
+	}
+	
+	// Bob ไม่ไหวแล้ว เปลี่ยนใจออกไป
+	line_up.remove("Bob");
+	
+	// 3 คนที่เข้าไปสัมมนาแล้ว (Luffy, Prayath, Tony)
+	line_up.remove("Luffy");
+	line_up.remove("Prayath");
+	line_up.remove("Tony");
+
+	// แสดงผลลัพธ์ตามที่ต้องการ
 	printList(line_up);
 		
 	return 0;
