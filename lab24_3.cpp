3a04fc3e7b5736c6dcb64ffa8b64b5db78e43881
sireethorn <sireethorn2548.h20p@gmail.com>
2025-03-04 15:36:12 +0700

    lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int size) {
+    set<int> uniqueNumbers(data, data + size); // ใช้ set เพื่อเก็บค่าที่ไม่ซ้ำกัน
+    return uniqueNumbers.size(); // คืนค่าจำนวนสมาชิกที่ไม่ซ้ำกัน
+}
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
